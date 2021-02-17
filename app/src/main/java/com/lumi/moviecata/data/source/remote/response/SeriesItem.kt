dc47c17960de4be77f251e6ea1d66f4b207fb876
lumi <lumipotato@gmail.com>
2021-02-17 17:56:42 +0700

    test unit n instrument

--- app/src/main/java/com/lumi/moviecata/data/source/remote/response/SeriesItem.kt
+++ app/src/main/java/com/lumi/moviecata/data/source/remote/response/SeriesItem.kt
@@ -7,39 +7,16 @@ import kotlinx.parcelize.Parcelize
 @Parcelize
 data class SeriesItem(
 
-	@field:SerializedName("first_air_date")
-	val firstAirDate: String? = null,
-
 	@field:SerializedName("overview")
 	val overview: String? = null,
 
-	@field:SerializedName("original_language")
-	val originalLanguage: String? = null,
-
-	@field:SerializedName("genre_ids")
-	val genreIds: List<Int?>? = null,
-
 	@field:SerializedName("poster_path")
 	val posterPath: String? = null,
-
-	@field:SerializedName("origin_country")
-	val originCountry: List<String?>? = null,
-
-	@field:SerializedName("original_name")
-	val originalName: String? = null,
-
-	@field:SerializedName("popularity")
-	val popularity: Double? = null,
-
-	@field:SerializedName("vote_average")
-	val voteAverage: Double? = null,
 
 	@field:SerializedName("name")
 	val name: String? = null,
 
 	@field:SerializedName("id")
-	val id: Int? = null,
+	val id: Int,
 
-	@field:SerializedName("vote_count")
-	val voteCount: Int? = null
 ):Parcelable
--- app/src/test/java/com/lumi/moviecata/viewmodel/RemoteDummy.kt
+++ app/src/test/java/com/lumi/moviecata/viewmodel/RemoteDummy.kt
@@ -1,6 +1,7 @@
 package com.lumi.moviecata.viewmodel
 
 import com.lumi.moviecata.data.source.remote.response.MovieItem
+import com.lumi.moviecata.data.source.remote.response.SeriesItem
 
 object RemoteDummy {
     fun getMovie() : List<MovieItem>{
@@ -127,6 +128,112 @@ object RemoteDummy {
                 null,
                 "Life Is Beautiful",null,20
             )
+        )
+    }
+
+    fun getSeries() : List<SeriesItem>{
+        return listOf(
+                SeriesItem(
+                        "I Am Not An Animal is an animated comedy series about the only six talking animals in the world, whose cosseted existence in a vivisection unit is turned upside down when they are liberated by animal rights activists.",
+                        null,
+                        "I Am Not an Animal",
+                        21
+                ),SeriesItem(
+                "Surrounded by a forest and a gated entrance, the Grace Field House is inhabited by orphans happily living together as one big family, looked after by their \\\"Mama,\\\" Isabella. Although they are required to take tests daily, the children are free to spend their time as they see fit, usually playing outside, as long as they do not venture too far from the orphanage — a rule they are expected to follow no matter what. However, all good times must come to an end, as every few months, a child is adopted and sent to live with their new family... never to be heard from again.\\n\\nHowever, the three oldest siblings have their suspicions about what is actually happening at the orphanage, and they are about to discover the cruel fate that awaits the children living at Grace Field, including the twisted nature of their beloved Mama.",
+                null,
+                "The Promised Neverland",
+                22
+        ),SeriesItem(
+                "Tightly clutching his Gibson guitar, Mafuyu Satou steps out of his dark apartment to begin another day of his high school life. While taking a nap in a quiet spot on the gymnasium staircase, he has a chance encounter with fellow student Ritsuka Uenoyama, who berates him for letting his guitar's strings rust and break. Noticing Uenoyama's knowledge of the instrument, Satou pleads for him to fix it and to teach him how to play. Uenoyama eventually agrees and invites him to sit in on a jam session with his two band mates: bassist Haruki Nakayama and drummer Akihiko Kaji.\\n\\nSatou's voice is strikingly beautiful, filling Uenoyama with the determination to make Satou the lead singer of the band. Though reticent at first, Satou takes the offer after an emotional meeting with an old friend. With the support of his new friends, Satou must not only learn how to play guitar, but also come to terms with the mysterious circumstances that led him to be its owner.",
+                null,
+                "Given",
+                23
+        ),SeriesItem(
+                "First comes marriage, then comes an earthbound love that is out of this world!\\n\\nFrom the day his parents named him, Nasa Yuzaki has been destined for outer space...or so he believes. But while trying to join the space program, he is thrown for a loop when an accident introduces him to Tsukasa, a mysterious girl with strange powers. Is she an alien, a moon goddess or something else? Since she insists on marrying him, Nasa has plenty of chances to find out!",
+                null,
+                "TONIKAWA: Over the Moon for You",
+                24
+        ),SeriesItem(
+                "Iwatani Naofumi was summoned into a parallel world along with 3 other people to become the world's Heroes. Each of the heroes respectively equipped with their own legendary equipment when summoned, Naofumi received the Legendary Shield as his weapon. Due to Naofumi's lack of charisma and experience he's labeled as the weakest, only to end up betrayed, falsely accused, and robbed by on the third day of adventure. Shunned by everyone from the king to peasants, Naofumi's thoughts were filled with nothing but vengeance and hatred. Thus, his destiny in a parallel World begins...",
+                null,
+                "The Rising of the Shield Hero",
+                25
+        ),SeriesItem(
+                "In a world of monsters, adventurers and magic, some of the most gifted healers are subjugated to brute force. Keyaru gains the ability to rewind time and turns the tables on those who’ve exploited him in this dark fantasy tale of vengeance and fury.",
+                null,
+                "Redo of Healer",
+                26
+        ),SeriesItem(
+                "Nature made Ash Lynx beautiful; nurture made him a cold ruthless killer. A runaway brought up as the adopted heir and sex toy of \\\"Papa\\\" Dino Golzine, Ash, now at the rebellious age of seventeen, forsakes the kingdom held out by the devil who raised him. But the hideous secret that drove Ash's older brother mad in Vietnam has suddenly fallen into Papa's insatiably ambitious hands—and it's exactly the wrong time for Eiji Okamura, a pure-hearted young photographer from Japan, to make Ash Lynx's acquaintance...",
+                null,
+                "Banana Fish",
+                27
+        ),SeriesItem(
+                "In today’s Japan, \\\"rental\\\" services can deliver an afternoon with a \\\"friend,\\\" a \\\"parent,\\\" even a fake girlfriend! Kinoshita Kazuya is a 20-year-old failure of a college student. He managed to kiss his girlfriend once, but was dumped after a month.  Completely spiteful, Kazuya gets just desperate enough to give it a try. But he quickly discovers how complicated it can be to \\\"rent\\\" an emotional connection… and his new \\\"girlfriend,\\\" Mizuhara Chizuru, who’s trying to keep her side hustle secret, will panic when she finds out her real life and Kazuya’s are intertwined in surprising ways! A reckless rom-com filled with love and excitement is about to begin!",
+                null,
+                "Rent-a-Girlfriend",
+                28
+        ),SeriesItem(
+                "Kisaragi Amatsuyu is invited out alone by the cool beauty upperclassman Cosmos and his childhood friend Himawari. Expecting to hear their confessions, he triumphantly goes to meet each of them in turn. But Cosmos and Himawari both instead confess to Amatsuyu that they like his friend. Amatsuyu fights this lonely battle, but there is another girl who is looking at him. She is a gloomy girl with glasses and braids. Amatsuyu finds that he hates her, because she's always turning her sharp tongue only on him and finding enjoyment in his troubles. But it turns out that she's the only one who actually does like him.",
+                null,
+                "ORESUKI: Are you the only one who loves me?",
+                29
+        ),SeriesItem(
+                "Kousei Arima was a genius pianist until his mother's sudden death took away his ability to play. Each day was dull for Kousei. But, then he meets a violinist named Kaori Miyazono who has an eccentric playing style. Can the heartfelt sounds of the girl's violin lead the boy to play the piano again?",
+                null,
+                "Your Lie in April",
+                30
+        ),SeriesItem(
+                "Seton Academy, a school full of animals where, thanks to population decline, there are fewer humans than any other creature. Mazama Jin, an animal hater and the only human male in his class, falls in love with Hino Hitomi, the only female human, the moment he lays eyes her. However he soon finds himself entangled with various other creatures after he reluctantly joins the 'pack' of Lanka the wolf, the only other member of her pack. After getting to know each other, the two decide to create a cooking club, and after a few bad-blooded misunderstandings, Ranka soon joins the club as well.\\n\\nThus begins the howl-some and howl-arious story of two normal humans; an adorable wolf; a cheerful koala; a sluggish, blonde sloth; and a feline with cattitude in their newfound club—in a story that teaches that friendship can be forged by creatures of different kinds.",
+                null,
+                "Seton Academy: Join the Pack!",
+                31
+        ),SeriesItem(
+                "Many years before becoming the famed captain of the Survey Corps, a young Levi struggles to survive in the capital's garbage dump, the Underground. As the boss of his own criminal operation, Levi attempts to get by with meager earnings while aided by fellow criminals, Isabel Magnolia and Farlan Church. With little hope for the future, Levi accepts a deal from the anti-expedition faction leader Nicholas Lobov, who promises the trio citizenship aboveground if they are able to successfully assassinate Erwin Smith, a squad leader of the Survey Corps.\\n\\nAs Levi and Erwin cross paths, Erwin acknowledges Levi's agility and skill and gives him the option to either become part of the expedition team, or be turned over to the Military Police, to atone for his crimes. Now closer to the man they are tasked to kill, the group plans to complete their mission and save themselves from a grim demise in the dim recesses of their past home. However, they are about to learn that the surface world is not as liberating as they had thought and that sometimes, freedom can come at a heavy price.\\n\\nBased on the popular spin-off manga of the same name, Attack on Titan: No Regrets illustrates the encounter between two of Attack on Titan's pivotal characters, as well as the events of the 23rd expedition beyond the walls.",
+                null,
+                "Attack on Titan: No Regrets",
+                32
+        ),SeriesItem(
+                "Yūri Katsuki carried all of Japan's hopes on his shoulders to win at the Gran Prix Finale ice skating competition, but suffered a crushing defeat. He returns home to Kyushu and half feels like he wants to retire, and half feels like he wants to continue ice skating. Suddenly the five-time consecutive world championship ice skater Victor Nikiforov appears before him with Yuri Plisetsky, a young Russian figure skater who is already defeating his seniors. Victor and both Yuris take up the challenge on an unprecedented Gran Prix series.",
+                null,
+                "Yuri!!! on Ice",
+                33
+        ),SeriesItem(
+                "Banri Tada is a freshman at a Tokyo law school. After an accident, he suffers severe memory loss. Despite the incident, he befriends fellow freshman, Mitsuo Yanagisawa, which leads him to the beautiful, yet obsessive, Kouko Kaga.",
+                null,
+                "Golden Time",
+                34
+        ),SeriesItem(
+                "In a world where eighty percent of the population has some kind of super-powered Quirk, Izuku was unlucky enough to be born completely normal. But that won’t stop him from enrolling in a prestigious hero academy. Now, he’ll get his first taste of brutal rivalry from other schools as he braves the cutthroat, no-holds-barred provisional license exam.",
+                null,
+                "My Hero Academia",
+                35
+        ),SeriesItem(
+                "Misaki Ayuzawa is the first female student council president at a once all-boys school turned co-ed. She rules the school with strict discipline demeanor. But she has a secret—she works at a maid cafe due to her families circumstances. One day the popular A-student and notorious heart breaker Takumi Usui finds out her secret and makes a deal with her to keep it hush from the school in exchange for spending some time with him.",
+                null,
+                "Maid Sama!",
+                36
+        ),SeriesItem(
+                "A spunky female weightlifter and free-spirited male swimmer meet on campus, only to find out their pasts may be intertwined.",
+                null,
+                "Weightlifting Fairy Kim Bok-Joo",
+                37
+        ),SeriesItem(
+                "Considered a genius due to having the highest grades in the country, Miyuki Shirogane leads the prestigious Shuchiin Academy's student council as its president, working alongside the beautiful and wealthy vice president Kaguya Shinomiya. The two are often regarded as the perfect couple by students despite them not being in any sort of romantic relationship.",
+                null,
+                "Kaguya-sama: Love is War",
+                38
+        ),SeriesItem(
+                "It is the Taishō period in Japan. Tanjirō, a kindhearted boy who sells charcoal for a living, finds his family slaughtered by a demon. To make matters worse, his younger sister Nezuko, the sole survivor, has been transformed into a demon herself. Though devastated by this grim reality, Tanjirō resolves to become a “demon slayer” so that he can turn his sister back into a human, and kill the demon that massacred his family.",
+                null,
+                "Demon Slayer: Kimetsu no Yaiba",
+                39
+        ),SeriesItem(
+                "Nariyuki Yuiga is in his last and most painful year of high school. In order to gain the “special VIP recommendation” which would grant him a full scholarship to college, he must now tutor his classmates as they struggle to prepare for entrance exams.\\n\\nAmong his pupils are “the sleeping beauty of the literary forest,” Fumino Furuhashi, and “the Thumbelina supercomputer,” Rizu Ogata–two of the most beautiful super-geniuses at the school! While these two were thought to be academically flawless, it turns out that they’re completely clueless outside of their pet subjects…!?\\n\\nAs Nariyuki’s life is turned upside down by these quirky girls who just never learn, he must do everything he can to get them accepted into college! The stage is set for this romantic comedy featuring prodigies who never learn when it comes to studying and love!",
+                null,
+                "We Never Learn: BOKUBEN",
+                40
+                )
         )
     }
 }
