f5fcc7a22dda71e1a615355105304b98d233de48
lumi <lumipotato@gmail.com>
2021-03-28 14:04:10 +0700

    add function to series

--- app/src/main/java/com/lumi/moviecata/ui/detail/DetailSeriesActivity.kt
+++ app/src/main/java/com/lumi/moviecata/ui/detail/DetailSeriesActivity.kt
@@ -1,8 +1,12 @@
 package com.lumi.moviecata.ui.detail
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import android.view.View
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
 import androidx.lifecycle.ViewModelProvider
 import com.bumptech.glide.Glide
 import com.bumptech.glide.load.resource.bitmap.RoundedCorners
@@ -14,6 +18,7 @@ import com.lumi.moviecata.databinding.ActivityDetailShowsBinding
 import com.lumi.moviecata.databinding.ContentDetailShowsBinding
 import com.lumi.moviecata.viewmodel.ViewModelFactory
 import com.lumi.moviecata.vo.Resource
+import com.lumi.moviecata.vo.Status
 
 class DetailSeriesActivity : AppCompatActivity() {
 
@@ -23,6 +28,7 @@ class DetailSeriesActivity : AppCompatActivity() {
 
     private lateinit var detailContentBinding: ContentDetailShowsBinding
     private lateinit var seriesViewModel: DetailSeriesViewModel
+    private var menu: Menu? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -39,7 +45,8 @@ class DetailSeriesActivity : AppCompatActivity() {
         val extras = intent.extras
         val seriesId = extras?.getInt(EXTRA_SERIES)
         if (seriesId != null) {
-            seriesViewModel.getSeriesDetail(seriesId).observe(this) { detail ->
+            seriesViewModel.setSelectedSeries(seriesId)
+            seriesViewModel.mSeries.observe(this) { detail ->
                 if (detail != null) {
                     populateSeries(detail)
                     showLoading(false)
@@ -60,6 +67,46 @@ class DetailSeriesActivity : AppCompatActivity() {
                 .apply(RequestOptions.placeholderOf(R.drawable.ic_loading)
                         .error(R.drawable.ic_error))
                 .into(detailContentBinding.imagePoster)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.menu_detail, menu)
+        this.menu = menu
+        seriesViewModel.mSeries.observe(this, { series ->
+            if (series != null) {
+                when (series.status) {
+                    Status.LOADING -> detailContentBinding.progressBar.visibility = View.VISIBLE
+                    Status.SUCCESS -> if (series.data != null) {
+                        detailContentBinding.progressBar.visibility = View.GONE
+                        val state = series.data.bookmarked
+                        setBookmarkState(state)
+                    }
+                    Status.ERROR -> {
+                        detailContentBinding.progressBar.visibility = View.GONE
+                        Toast.makeText(applicationContext, "Terjadi kesalahan", Toast.LENGTH_SHORT).show()
+                    }
+                }
+            }
+        })
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        if (item.itemId == R.id.action_fav) {
+            seriesViewModel.setBookmark()
+            return true
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    private fun setBookmarkState(state: Boolean) {
+        if (menu == null) return
+        val menuItem = menu?.findItem(R.id.action_fav)
+        if (state) {
+            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_baseline_favorite_36)
+        } else {
+            menuItem?.icon = ContextCompat.getDrawable(this, R.drawable.ic_outline_favorite_border_36)
+        }
     }
 
     private fun showLoading(state: Boolean) {
--- app/src/main/java/com/lumi/moviecata/ui/detail/DetailSeriesViewModel.kt
+++ app/src/main/java/com/lumi/moviecata/ui/detail/DetailSeriesViewModel.kt
@@ -1,6 +1,8 @@
 package com.lumi.moviecata.ui.detail
 
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.Transformations
 import androidx.lifecycle.ViewModel
 import com.lumi.moviecata.data.source.MovieCataRepository
 import com.lumi.moviecata.data.source.local.entity.SeriesEntity
@@ -8,6 +10,27 @@ import com.lumi.moviecata.vo.Resource
 
 class DetailSeriesViewModel (private val movieCataRepository: MovieCataRepository) : ViewModel(){
 
-    fun getSeriesDetail(seriesId: Int) : LiveData<Resource<SeriesEntity>> = movieCataRepository.getSeriesDetail(seriesId)
+    private val seriesId = MutableLiveData<Int>()
+
+    fun setSelectedSeries(seriesId: Int) {
+        this.seriesId.value = seriesId
+    }
+
+    var mSeries: LiveData<Resource<SeriesEntity>> =
+            Transformations.switchMap(seriesId) { mSeriesId ->
+                movieCataRepository.getSeriesDetail(mSeriesId)
+            }
+
+    fun setBookmark() {
+        val seriesResource = mSeries.value
+        if (seriesResource != null) {
+            val seriesEntity = seriesResource.data
+
+            if (seriesEntity != null) {
+                val newState = !seriesEntity.bookmarked
+                movieCataRepository.setSeriesBookmark(seriesEntity, newState)
+            }
+        }
+    }
 
 }
